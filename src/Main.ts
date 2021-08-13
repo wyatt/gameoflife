a99ad0b578cf7911d1ef434f6a246a57ffa35642
Wyatt Sell <profwyattb@gmail.com>
2021-08-13 22:46:53 +0100

    Finally working!

--- src/Main.ts
+++ src/Main.ts
@@ -1,20 +1,16 @@
 import Phaser from 'phaser';
-
-import PreloaderScene from './scenes/PreloaderScene';
 import InitialScene from './scenes/InitialScene';
 
 const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    width: 800,
-    height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 500 }
-        }
-    },
-    scene: [PreloaderScene, InitialScene],
-    backgroundColor: '#21213B'
+	parent: 'phaser',
+	type: Phaser.CANVAS,
+	width: 500,
+	height: 360,
+	scale: {
+		mode: Phaser.Scale.WIDTH_CONTROLS_HEIGHT,
+	},
+	scene: [InitialScene],
+	backgroundColor: '#21213B',
 };
 
 export default new Phaser.Game(config);
--- src/scenes/InitialScene.ts
+++ src/scenes/InitialScene.ts
@@ -1,19 +1,87 @@
 import Phaser from 'phaser';
+import {gospherGliderGun} from '../presets';
+
+type CellType =
+	| {
+			alive: true;
+			x: number;
+			y: number;
+			sprite: Phaser.GameObjects.Rectangle;
+	  }
+	| {alive: false; x: number; y: number};
 
 export default class InitialScene extends Phaser.Scene {
-    private achoThePup!: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+	private cells!: CellType[];
 
-    constructor() {
-        super('initial');
-    }
+	constructor() {
+		super('initial');
+	}
 
-    create(): void {
+	create(): void {
+		this.cells = [...Array(50)]
+			.map((_item, x) => {
+				return [...Array(36)].map((_item, y) => {
+					return [x, y];
+				});
+			})
+			.flat(1)
+			.map(([x, y]) => {
+				const alive = !!gospherGliderGun.find(
+					([mx, my]) => mx === x && my === y
+				);
+				return {
+					x,
+					y,
+					alive,
+					...(alive && {
+						sprite: this.add
+							.rectangle(x * 10, y * 10, 10, 10, 0xf44336)
+							.setOrigin(0, 0),
+					}),
+				} as CellType;
+			});
 
-        this.add.image(400, 570, 'ground');
-        this.achoThePup = this.physics.add.image(0, 0, 'acho');
-        
-        this.achoThePup.setCollideWorldBounds(true);
-        this.achoThePup.setBounce(1, 1);
-        this.achoThePup.setVelocityX(300);
-    }
+		this.time.addEvent({
+			delay: 200,
+			callback: this.generation.bind(this),
+			loop: true,
+		});
+	}
+
+	generation(): void {
+		const currentCells = this.cells;
+		this.cells = currentCells.map(cell => {
+			const liveNeighbours = currentCells.filter(neighbour => {
+				return (
+					(cell.x - 1 === neighbour.x ||
+						cell.x === neighbour.x ||
+						cell.x + 1 === neighbour.x) &&
+					(cell.y - 1 === neighbour.y ||
+						cell.y === neighbour.y ||
+						cell.y + 1 === neighbour.y) &&
+					!(cell.y === neighbour.y && cell.x === neighbour.x) &&
+					neighbour.alive
+				);
+			});
+
+			if (cell.alive) {
+				if (!(liveNeighbours.length === 2 || liveNeighbours.length === 3)) {
+					cell.sprite.destroy();
+					return {x: cell.x, y: cell.y, alive: false};
+				}
+			} else {
+				if (liveNeighbours.length === 3) {
+					return {
+						x: cell.x,
+						y: cell.y,
+						alive: true,
+						sprite: this.add
+							.rectangle(cell.x * 10, cell.y * 10, 10, 10, 0xf44336)
+							.setOrigin(0, 0),
+					};
+				}
+			}
+			return cell;
+		});
+	}
 }
